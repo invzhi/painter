69bbfed61dbbf9a384bf1cfa9983f3b9031c785d
invzhi <invzhi@protonmail.com>
2018-01-02 21:08:35 +0800

    Modified frontend

--- static/monitor.js
+++ static/monitor.js
@@ -1,39 +1,52 @@
 window.onload = function() {
   var list = document.getElementById('list');
+  var tips = document.getElementById('tips');
 
   if (window.WebSocket) {
     var conn = new WebSocket('ws://' + document.location.host + document.location.pathname + 'ws');
 
     conn.onopen = function(event) {
-      console.log('websocket opened')
-      document.getElementById('start').onclick = function() {
-        conn.send('start');
-      };
-      document.getElementById('end').onclick = function() {
-        conn.send('end');
-      };
-    }
+      console.log('websocket opened.');
+      document.getElementById('start').onclick = function() { conn.send('start'); };
+      document.getElementById('end').onclick = function() { conn.send('end'); };
+    };
+
     conn.onclose = function(event) {
       console.log('websocket closed');
-    }
+    };
+
     conn.onmessage = function(event) {
       console.log(event.data);
+
       var msg = JSON.parse(event.data);
       switch (msg.code) {
-        case 0: // join
-          item = document.createElement("li");
-          item.innerHTML = msg.username + ': <span id="' + msg.username + '">0</span>';
-          list.appendChild(item);
+        case 0:
+          console.log(msg.username, 'join');
+          appendUser(msg.username)
           break;
-        case 1: // shake
+        case 1:
+          console.log(msg.username, 'shake');
           document.getElementById(msg.username).innerHTML++;
           break;
       }
-    }
+    };
+
     conn.onerror = function(event) {
       console.log('something error');
-    }
+    };
   } else {
-    alert('You browser is holy shit, just delete it and install firefox or chrome!')
+    tips.innerHTML = 'You browser is holy shit, just delete it and install firefox or chrome!';
   }
+}
+
+function appendUser(username) {
+  var item = document.createElement("li");
+  var label = document.createElement('label');
+  var number = document.createElement('span');
+  label.innerHTML = username;
+  number.id = username;
+  number.innerHTML = 0;
+  item.appendChild(label);
+  item.appendChild(number);
+  list.appendChild(item);
 }
--- static/start.js
+++ static/start.js
@@ -1,59 +1,83 @@
 window.onload = function() {
-  var state = 0; // unconnected
-  var conn;
-  var num = document.getElementById('times')
+  var conn, number;
   var form = document.getElementById('form');
+  var tips = document.getElementById('tips');
 
   form.onsubmit = function() { return false; };
 
   if (window.DeviceMotionEvent && window.WebSocket) {
     conn = new WebSocket('ws://' + document.location.host + document.location.pathname + 'ws');
+
     conn.onopen = function(event) {
-      console.log('websocket opened')
+      console.log('websocket opened.');
+
       form.onsubmit = function() {
         var username = document.getElementById('username');
-        if (conn && username.value) {
+        if (username.value) {
           conn.send(username.value);
+          tips.innerHTML = 'You have a name: ' + username.value;
+          number = document.createElement('b');
+          number.innerHTML = 0;
+          document.getElementsByTagName('body')[0].insertBefore(number, form);
+          form.remove();
+          form = number;
+        } else {
+          tips.innerHTML = 'cannot be a name';
         }
         return false;
       };
     };
-    conn.onclose = function(event) {
-      console.log('websocket closed')
-    };
+
     conn.onmessage = function(event) {
-      console.log('receive data: ', event.data)
+      console.log('receive data: ', event.data);
       switch (event.data) {
         case "start":
-          state = 1;
           window.addEventListener('devicemotion', deviceMotionHandler);
+          tips.innerHTML = 'Game Start';
           break;
         case "end":
-          state = 0;
           window.removeEventListener('devicemotion', deviceMotionHandler);
+          tips.innerHTML = 'Game End';
           break;
       }
     };
+
+    conn.onclose = function(event) {
+      console.log('websocket closed.');
+      tips.innerHTML = 'you are offline.';
+    };
+
     conn.onerror = function(event) {
-      console.log('something error')
+      console.log('websocket error.');
+      tips.innerHTML = 'you have trouble.'
     };
   } else {
-    alert('You browser is holy shit, just delete it and install firefox or chrome!');
+    tips.innerHTML = 'You browser is holy shit, just delete it and install better one!';
   }
 
-  function deviceMotionHandler(event) {
-    var speed = 40;
-    var x, y, z, lastX, lastY, lastZ;
-    x = y = z = lastX = lastY = lastZ = 0;
+  var SHAKE_THRESHOLD = 800;
+  var last_update = 0;
+  var x = y = z = last_x = last_y = last_z = 0;
 
-    var acceleration = event.accelerationIncludingGravity;
-    x = acceleration.x;
-    y = acceleration.y;
-    if(Math.abs(x-lastX) > speed || Math.abs(y-lastY) > speed) {
-      conn.send("shake");
-      num.innerHTML++;
+  function deviceMotionHandler(eventData) {
+    var acceleration = eventData.accelerationIncludingGravity;
+    var curTime = new Date().getTime();
+    
+    if (curTime - last_update > 100) {
+      var diffTime = curTime - last_update;
+      last_update = curTime;
+      x = acceleration.x;
+      y = acceleration.y;
+      z = acceleration.z;
+      var speed = Math.abs(x + y + z - last_x - last_y - last_z) / diffTime * 10000;
+      
+      if (speed > SHAKE_THRESHOLD) {
+        conn.send("shake");
+        number.innerHTML++;
+      }
+      last_x = x;
+      last_y = y;
+      last_z = z;
     }
-    lastX = x;
-    lastY = y;
   }
 }
